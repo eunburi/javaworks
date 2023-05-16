c2f655316a5f1d50db3b753f01c8c724a0650428
eunburi <finestar23@naver.com>
2023-05-16 13:17:02 +0900

    연산자

--- ch01/src/var/ByteShortType.java
+++ ch01/src/var/ByteShortType.java
@@ -3,11 +3,11 @@ package var; //
 public class ByteShortType {
 
 	public static void main(String[] args) {
-		// Byte형
-		// short형 (-32768 ~ 32767)
-		
+		// Byte형(~128 ~ 127)0 - 8비트, 256
+		// short형 (-32768 ~ 32767) -16비트, 65536 - > 유니코드
 		byte bData1 = -128;
-		//byte bData2 = 128; //127을 넘어서옹류
+		//byte bData2 = 128; //127을 넘어서 오류(오버플로우 : over flow)
+		
 		
 		short sData1 = 128;
 		short sData2 = 32767;
