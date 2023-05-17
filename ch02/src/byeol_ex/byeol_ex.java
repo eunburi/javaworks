8303f13d355b05e7f88582a50e72c515a5fdfe93
eunburi <finestar23@naver.com>
2023-05-17 14:33:01 +0900

    switch, for

---
+++ ch02/src/byeol_ex/byeol_ex.java
@@ -0,0 +1,14 @@
+package byeol_ex;
+
+public class byeol_ex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name;
+		int hour = 15;
+		name = "신짱구";
+		System.out.println(name + "님 배송이 " + hour +"시에 시작되었습니다.");
+		
+	}
+
+}
---
+++ ch02/src/forexample/Gugudan.java
@@ -0,0 +1,18 @@
+package forexample;
+
+public class Gugudan {
+
+	public static void main(String[] args) {
+		// 전체 구구단 출력
+
+		
+		
+		int dan = 1;
+		
+		for(int i=1; i<10; i++) {
+			System.out.println(dan +"x" + i +"="+(dan*i));}
+		
+		
+	}
+
+}
