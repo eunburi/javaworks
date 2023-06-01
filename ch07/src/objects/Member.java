1c238b34c3b57a24b82a53d56092c039389c1108
eunburi <finestar23@naver.com>
2023-06-01 18:20:53 +0900

    0601 Object

---
+++ ch07/src/objects/Member.java
@@ -0,0 +1,40 @@
+package objects;
+
+public class Member {
+	
+	String id;
+	
+	Member(String id){
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return id.hashCode(); // 반환형 int형이므로 hashCode()호출
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Member) {
+			Member member = (Member)obj;
+			if(id.equals(member.id)) {
+				return true;
+			}
+			
+		}
+		return false;
+		
+
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
