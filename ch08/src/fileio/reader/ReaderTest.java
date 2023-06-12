b3d52a3a77609283e6557904bec93c6ff04b10d0
eunburi <finestar23@naver.com>
2023-06-12 11:20:00 +0900

    ch 08 file_io image copy

---
+++ ch08/src/fileio/reader/ReaderTest.java
@@ -0,0 +1,34 @@
+package fileio.reader;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class ReaderTest {
+
+	public static void main(String[] args) throws IOException {
+		
+		Reader fr = new FileReader("data2.txt");
+				
+		
+		 int data; 
+		 /*while(true) {
+			 data = fr.read();	//코드값으로 저장
+			 if(data == -1) // 읽을 데이터가 없으면
+				 break;		// 빠져 나옴
+			 System.out.print((char)data); //문자로 출력
+			 // 아스키코드값을 불러오기 때문에
+			 								//char 앞에 붙여줘야함!
+		 }*/
+		
+		while((data = fr.read()) != -1) {
+			 System.out.print((char)data);
+		}
+			
+		
+		 fr.close();
+		 
+
+	}
+
+}
---
+++ ch08/src/fileio/writer/WriterTest.java
@@ -0,0 +1,30 @@
+package fileio.writer;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+public class WriterTest {
+
+	public static void main(String[] args) throws IOException {
+		
+		Writer fw = new FileWriter("c:/file/newFile2.txt");
+		
+		fw.write("hello~\n");
+		fw.write("안눙\n");
+		fw.write(66); //정수는 아스키 코드값으로 인식 - >  B(아스키코드) 
+		fw.write(66+1);
+		// fw.write(2.54); - 실수 사용불가
+		
+		char[] buf = {'s','m','i','l','e'};
+		fw.write(buf);
+		fw.write(9); //줄바꿈(개행문자)
+		fw.write(buf, 1, 4); // 1번 인덱스부터 4개 출력
+		
+		fw.close();
+		
+		System.out.println("수행완료!!");
+
+	}
+
+}
